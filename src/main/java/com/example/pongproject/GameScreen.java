57441d8df063abdf0cac5d4fa977fccdf9ce2427
z00506rx <lorenz.schneider04@gmail.com>
2025-01-23 13:16:13 +0100

    Final V1

--- src/main/java/com/example/pongproject/GameScreen.java
+++ src/main/java/com/example/pongproject/GameScreen.java
@@ -6,7 +6,6 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.input.KeyCode;
